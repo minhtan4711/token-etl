f81a80b116b37c2b8ec2612a00177ed3d0ab1e8d
minhtan <32776120+minhtan4711@users.noreply.github.com>
2023-08-02 04:45:49 +0700

    add token address

--- src/main/scala/constants/Common.scala
+++ src/main/scala/constants/Common.scala
@@ -3,6 +3,8 @@ package constants
 object Common {
   val WHALE_THRESHOLD = 0.0005
   val valas = "0xb1ebdd56729940089ecc3ad0bbeeb12b6842ea6f"
+  val rad = "0xf7de7e8a6bd59ed41a4b5fe50278b3b7f31384df"
+  val venus = "0xcf6bb5389c92bdda8a3747ddb454cb7a64626c63"
   val LOW_NUMBER_OF_TRANSFER = 100
   val MEDIUM_NUMBER_OF_TRANSFER = 1000
 }
